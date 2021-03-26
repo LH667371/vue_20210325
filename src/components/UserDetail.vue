b977efeb9e4ca86c5a509aafdb494a10634b7a1c
LH667371 <773426798@qq.com>
2021-03-26 10:34:13 +0800

    更新；用户详情页显示为input

--- src/components/UserDetail.vue
+++ src/components/UserDetail.vue
@@ -1,16 +1,20 @@
 <template>
     <div>
         <h3>用户详情页</h3>
-<!--        需求：在用户详情页展示该用户的详细信息-&ndash;&gt; {{ JSON.parse($route.params.data)  }}-->
-        <!--    遍历查询用户的详细信息  展示出来    -->
+        <!-- 需求：在用户详情页展示该用户的详细信息-&ndash;&gt; {{ JSON.parse($route.params.data)  }}-->
+        <!-- 遍历查询用户的详细信息  展示出来    -->
         <span v-for="(value, key) in JSON.parse($route.params.data)" :key="key">
-            {{ key }}: {{ value }}<br>
+            <span v-if="key==='id'">
+                {{ key }}: <input type="text" :value="value" readonly><br>
+            </span>
+            <span v-else>
+                {{ key }}: <input type="text" :value="value"><br>
+            </span>
         </span>
         <button @click="back_index">返回首页</button>
         <button @click="back_user_info">返回用户列表页</button>
     </div>
 </template>
-
 <script>
 export default {
     name: "UserDetail",
@@ -22,7 +26,7 @@ export default {
             this.$router.push({path: '/'});
         },
         back_user_info() {
-          this.$router.push({path: '/second'});
+            this.$router.push({path: '/second'});
         },
     }
 }
